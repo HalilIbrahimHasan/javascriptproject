8a2ded815c512bfc50a94db8a48ca18e31d2135b
Ibrahim <hisetkal@gmail.com>
2022-02-08 18:06:34 -0500

    all

---
+++ EvenOdd.js
@@ -0,0 +1,15 @@
+
+function isEven(){
+
+    var number = document.getElementById("TextBox").value;
+    
+    if(number %2 === 0){
+    
+    alert(number + " number is even!");
+    
+    }else{
+      alert(number + " number is odd!");
+    }
+    
+    }
+    
